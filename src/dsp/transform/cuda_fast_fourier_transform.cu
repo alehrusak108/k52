bdb71ca9def9f32ca9452960587a53160f345482
Aleh_Rusak <pro.olegrusak@gmail.com>
2017-05-11 17:10:28 +0300

    Cuda FFT Implementation improvements. Used multiple devices.

--- src/dsp/transform/cuda_fast_fourier_transform.cu
+++ src/dsp/transform/cuda_fast_fourier_transform.cu
@@ -175,8 +175,10 @@ public:
         std::cout << std::endl << "Copying memory from device to device... " << std::endl;
 
         cudaLibXtDesc *device_signal;
-        std::cout << std::endl << cufftXtMalloc(cufft_execution_plan_, &device_signal, CUFFT_XT_FORMAT_INPLACE) << std::endl;
-        std::cout << std::endl << cufftXtMemcpy(cufft_execution_plan_, device_signal, input_signal, CUFFT_COPY_DEVICE_TO_DEVICE); << std::endl;
+        cufftResult malloc_result = cufftXtMalloc(cufft_execution_plan_, &device_signal, CUFFT_XT_FORMAT_INPLACE);
+        cufftResult memcpy_result = cufftXtMemcpy(cufft_execution_plan_, device_signal, input_signal, CUFFT_COPY_DEVICE_TO_DEVICE);
+        std::cout << std::endl << malloc_result << std::endl;
+        std::cout << std::endl << memcpy_result << std::endl;
 
         std::cout << std::endl << "CUFFT INVERSE Signal memory allocated across GPUs: " << signal_memory_size_ << " bytes." << std::endl;
 

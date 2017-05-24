eea02e72b826639c99142599ffc36c8365ecc3c2
Aleh_Rusak <pro.olegrusak@gmail.com>
2017-05-24 19:39:01 +0300

    CUDA memory optimizations.

--- examples/fourier_based_circular_convolution/fft_performance_examples.cu
+++ examples/fourier_based_circular_convolution/fft_performance_examples.cu
@@ -88,7 +88,7 @@ int main(int argc, char* argv[]) {
     ofstream test_output;
     test_output.open("fast_fourier_transform_test.txt", ios::out | ios::app);
     test_output << endl << "FFT PERFORMANCE TEST (FFTW vs CUDA)" << endl << endl;
-    int signal_size = 268435456;
+    int signal_size = 536870912;
     //for (int test_index = 1; test_index <= 8; test_index++) {
         vector<complex<double> > input_signal = Helpers::GenerateComplexSignal(signal_size);
         //test_output << endl << "TEST #" << test_index << "\t" << "Signal Length is: " << signal_size << endl;

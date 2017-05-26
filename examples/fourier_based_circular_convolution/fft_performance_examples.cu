bfe8ce0f7f804399a38e2aa77ac2afb46bc99d01
Aleh_Rusak <pro.olegrusak@gmail.com>
2017-05-26 12:16:35 +0300

    CUDA memory optimizations.

--- examples/fourier_based_circular_convolution/fft_performance_examples.cu
+++ examples/fourier_based_circular_convolution/fft_performance_examples.cu
@@ -33,7 +33,7 @@ void CUFFTPerformanceTest(vector<complex<double> > input_signal)
     test_output.open("fast_fourier_transform_test.txt", ios::out | ios::app);
     test_output << endl << "[ CUFFT Performance TEST ] STARTED." << endl;
 
-    size_t page_size = 8;
+    size_t page_size = 262144;
 
     clock_t planning_time = clock();
     CudaFastFourierTransform cufftTransformer(input_signal, page_size);
@@ -44,7 +44,7 @@ void CUFFTPerformanceTest(vector<complex<double> > input_signal)
     cufftTransformer.DirectTransform();
     vector<complex<double> > output = cufftTransformer.GetTransformResult();
 
-    Helpers::PrintComplexVector(output);
+    //Helpers::PrintComplexVector(output);
 
     clock_t finish = clock() - execution_time;
     test_output << endl << "Time elapsed for CUFFT Transform Test: " << (double) (clock() - execution_time) / CLOCKS_PER_SEC << " seconds " << endl << endl;
@@ -59,7 +59,7 @@ void FFTWPerformanceTest(vector<complex<double> > input_signal)
     test_output.open("fast_fourier_transform_test.txt", ios::out | ios::app);
     test_output << "[ FFTW3 Performance TEST ] STARTED." << endl;
 
-    size_t page_size = 131072;
+    size_t page_size = 262144;
     int total_pages = input_signal.size() / page_size;
 
     clock_t planning_time = clock();
@@ -88,15 +88,15 @@ int main(int argc, char* argv[]) {
     ofstream test_output;
     test_output.open("fast_fourier_transform_test.txt", ios::out | ios::app);
     test_output << endl << "FFT PERFORMANCE TEST (FFTW vs CUDA)" << endl << endl;
-    int signal_size = 64;
-    //for (int test_index = 1; test_index <= 7; test_index++) {
+    int signal_size = 2097152;
+    for (int test_index = 1; test_index <= 7; test_index++) {
         vector<complex<double> > input_signal = Helpers::GenerateComplexSignal(signal_size);
-      //  test_output << endl << "TEST #" << test_index << "\t" << "Signal Length is: " << signal_size << endl;
+        test_output << endl << "TEST #" << test_index << "\t" << "Signal Length is: " << signal_size << endl;
         CUFFTPerformanceTest(input_signal);
         test_output << "-----------------------------------------------------------------------" << endl << endl;
-        //FFTWPerformanceTest(input_signal);
+        FFTWPerformanceTest(input_signal);
         test_output << "===============================================================================" << endl << endl;
         signal_size *= 2;
-    //}
+    }
     test_output.close();
 }
